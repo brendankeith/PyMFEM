f32ee9a411c62ee555f19b1b72076e657e77b7c3
Justincrum <jcrum@email.arizona.edu>
2021-06-22 09:20:03 -0700

    HP refinement environement and test script.

---
+++ RLModule/examples/Test7_Prefinement.py
@@ -0,0 +1,149 @@
+"""
+    
+    Script to train an optimal marking policy starting from a fixed initial mesh
+
+    * In order for this to work with rllib, you need to have RLModule in your PYTHONPATH
+
+"""
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import ray
+import ray.rllib.agents.ppo as ppo
+from ray.tune.registry import register_env
+from prob_envs.HpProblem import HpProblem
+# from prob_envs.VariableInitialMesh import VariableInitialMesh
+import numpy as np
+# from prob_envs.FixedInitialMesh import FixedInitialMesh
+from drawnow import drawnow, figure
+
+scores = []
+avg_scores = []
+def draw_fig():
+  plt.title('reward')
+  plt.plot(scores, 'r-')
+  plt.plot(avg_scores, 'b-')
+
+prob_config = {
+    # 'mesh_name'         : 'star.mesh',
+    'mesh_name'         : 'l-shape.mesh',
+    'num_unif_ref'      : 1,
+    # 'num_random_ref'    : 2,
+    'order'             : 2,
+    'optimization_type' : 'error_threshold', # 'error_threshold', 'dof_threshold', 'step_threshold'
+    # 'random_mesh'       : True
+}
+
+total_episodes = 4000
+batch_size = 16
+nbatches = int(total_episodes/batch_size)
+checkpoint_period = 200
+
+config = ppo.DEFAULT_CONFIG.copy()
+config['train_batch_size'] = batch_size
+config['sgd_minibatch_size'] = batch_size
+config['rollout_fragment_length'] = batch_size
+config['num_workers'] = 3
+config['num_gpus'] = 0
+config['gamma'] = 1.0
+config['lr'] = 1e-4
+
+
+ray.shutdown()
+ray.init(ignore_reinit_error=True)
+env = HpProblem(**prob_config)
+register_env("my_env", lambda config : HpProblem(**prob_config))
+agent = ppo.PPOTrainer(env="my_env", config=config)
+
+episode = 0
+checkpoint_episode = 0
+for n in range(nbatches):
+    print("training batch %d of %d batches" % (n+1,nbatches))
+    result = agent.train()
+    episode += config['train_batch_size']
+    checkpoint_episode += config['train_batch_size']
+    episode_score = -result["episode_reward_mean"]
+    print ("Episode cost", episode_score)
+    # scores.append(episode_score)
+    # if n == 0:
+    #     avg_scores.append(episode_score)
+    # else:
+    #     avg_scores.append(avg_scores[-1] * 0.99 + episode_score*0.01)
+    
+    # drawnow(draw_fig)
+    if (checkpoint_episode >= checkpoint_period and n > 0.9*(nbatches-1)):
+        checkpoint_episode = 0
+        checkpoint_path = agent.save()
+        print(checkpoint_path)    
+
+
+root_path, _ = os.path.split(checkpoint_path)
+root_path, _ = os.path.split(root_path)
+csv_path = root_path + '/progress.csv'
+df = pd.read_csv(csv_path)
+cost = -df.episode_reward_mean.to_numpy()
+
+fig, ax = plt.subplots(2)
+ax[0].plot(cost,'r',lw=1.3)
+# ax.semilogy(cost,'r',lw=1.3)
+ax[0].set_ylabel("cost")
+ax[0].set_xlabel("iteration")
+# plt.show()
+
+agent.restore(checkpoint_path)
+
+
+prob_config['random_mesh'] = False
+
+## print
+import time
+prob_config['num_random_ref'] = 0
+episode_cost = 0
+done = False
+obs = env.reset()
+print("Num. Elems. = ", env.mesh.GetNE())
+env.render()
+rlcost = 0
+rlactions = []
+while not done:
+    action = agent.compute_action(obs,explore=False)
+    rlactions.append(action)
+    obs, reward, done, info = env.step(action)
+    episode_cost -= reward 
+    rlcost = episode_cost
+    print("step = ", env.k)
+    print("action = ", action.item())
+    print("Num. Elems. = ", env.mesh.GetNE())
+    print("episode cost = ", episode_cost)
+    time.sleep(0.05)
+    env.RenderMesh()
+
+
+costs = []
+rlcosts = []
+actions = []
+nth = 11
+for i in range(1, nth):
+    action = np.array([i/(nth-1)])
+    actions.append(action.item())
+    rlcosts.append(rlcost)
+    env.reset()
+    done = False
+    episode_cost = 0
+    while not done:
+        _, reward, done, info = env.step(action)
+        episode_cost -= reward 
+        print("step = ", env.k)
+        print("action = ", action.item())
+        print("Num. Elems. = ", env.mesh.GetNE())
+        print("episode cost = ", episode_cost)
+    costs.append(episode_cost)
+
+ax[1].plot(actions,costs,'-or',lw=1.3)
+ax[1].plot(actions,rlcosts,'-b',lw=1.3)
+# ax.semilogy(cost,'r',lw=1.3)
+ax[1].set_ylabel("cost")
+ax[1].set_xlabel("Constant Actions (theta)")
+
+plt.show()
---
+++ RLModule/prob_envs/HpProblem.py
@@ -0,0 +1,231 @@
+import os
+from os.path import expanduser, join
+import gym
+from gym import spaces
+import numpy as np
+import mfem.ser as mfem
+from mfem.ser import intArray
+from utils.StatisticsAndCost import Statistics, GlobalError
+
+class HpProblem(gym.Env):
+
+    def __init__(self,**kwargs):
+        super().__init__()
+        self.BC = mfem.ConstantCoefficient(0.0)
+        self.RHS = mfem.ConstantCoefficient(1.0)
+        self.coeff = mfem.ConstantCoefficient(1.0)
+
+        self.optimization_type = kwargs.get('optimization_type','error_threshold')
+        self.error_threshold = kwargs.get('error_threshold',1e-3)
+        self.dof_threshold = kwargs.get('dof_threshold',1e4)
+        self.step_threshold = kwargs.get('step_threshold',10)
+        mesh_name = kwargs.get('mesh_name','l-shape.mesh')
+        num_unif_ref = kwargs.get('num_unif_ref',1)
+        order = kwargs.get('order',1)    
+        meshfile = expanduser(join(os.path.dirname(__file__), '../..', 'data', mesh_name))
+        mesh = mfem.Mesh(meshfile)
+        mesh.EnsureNCMesh()
+        for _ in range(num_unif_ref):
+            mesh.UniformRefinement()
+        self.dim = mesh.Dimension()
+        self.initial_mesh = mesh
+        self.order = order
+        self.action_space = spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32)
+#        self.action_space = spaces.Dict({"space" : spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32), 
+#                                         "order" : spaces.Discrete(2)})
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(5,))
+    
+    def reset(self):
+        self.k = 0
+        self.mesh = mfem.Mesh(self.initial_mesh)
+        self.Setup()
+        self.AssembleAndSolve()
+        self.errors = self.GetLocalErrors()
+        obs = self.Errors2Observation(self.errors)
+        self.global_error = GlobalError(self.errors)
+        self.sum_of_dofs = self.fespace.GetTrueVSize()
+        return obs
+    
+    def step(self, action):
+        self.k += 1
+        self.UpdateMesh(action)
+        self.AssembleAndSolve()
+        self.errors = self.GetLocalErrors()
+        obs = self.Errors2Observation(self.errors)
+        if self.optimization_type == 'error_threshold':
+            global_error = GlobalError(self.errors)
+            num_dofs = self.fespace.GetTrueVSize()
+            cost = np.log(1.0 + num_dofs/self.sum_of_dofs)
+            self.sum_of_dofs += num_dofs
+            if global_error < self.error_threshold:
+                done = True
+            else:
+                done = False
+        elif self.optimization_type == 'dof_threshold':
+            self.sum_of_dofs += self.fespace.GetTrueVSize()
+            if self.sum_of_dofs > self.dof_threshold:
+                cost = 0.0
+                done = True
+            else:
+                global_error = GlobalError(self.errors)
+                cost = np.log(global_error/self.global_error)
+                self.global_error = global_error
+                done = False
+        else:
+            if self.k == 1:
+                accumulated_cost = 0
+            else:
+                accumulated_cost = np.log(self.global_error*self.sum_of_dofs)
+            self.global_error = GlobalError(self.errors)
+            self.sum_of_dofs += self.fespace.GetTrueVSize()
+            cost = np.log(self.global_error*self.sum_of_dofs) - accumulated_cost
+            if self.k >= self.step_threshold:
+                done = True
+            elif self.sum_of_dofs > self.dof_threshold:
+                cost = 10.0
+                done = True
+            else:
+                done = False
+        info = {}
+        return obs, -cost, done, info
+    
+    def render(self):
+        sol_sock = mfem.socketstream("localhost", 19916)
+        sol_sock.precision(8)
+        sol_sock.send_solution(self.mesh,  self.x)
+        title = "step " + str(self.k)
+        sol_sock.send_text("window_title '" + title)
+
+    def Setup(self):
+        # print("Setting up Poisson problem ")
+        dim = self.mesh.Dimension()
+        fec = mfem.H1_FECollection(self.order, dim)
+        self.fespace = mfem.FiniteElementSpace(self.mesh, fec)
+        self.a = mfem.BilinearForm(self.fespace)
+        self.b = mfem.LinearForm(self.fespace)
+        integ = mfem.DiffusionIntegrator(self.coeff)
+        self.a.AddDomainIntegrator(integ)
+        self.b.AddDomainIntegrator(mfem.DomainLFIntegrator(self.RHS))
+        self.x = mfem.GridFunction(self.fespace)
+        self.x.Assign(0.0)
+        self.ess_bdr = intArray(self.mesh.bdr_attributes.Max())
+        self.ess_bdr.Assign(1)
+        self.x.ProjectBdrCoefficient(self.BC, self.ess_bdr)
+        self.flux_fespace = mfem.FiniteElementSpace(self.mesh, fec, dim)
+        self.estimator =  mfem.ZienkiewiczZhuEstimator(integ, self.x, self.flux_fespace,
+                                                       own_flux_fes = False)
+
+        self.refiner = mfem.ThresholdRefiner(self.estimator)
+
+    def Errors2Observation(self, errors):
+        stats = Statistics(errors)
+        obs = [stats.nels, stats.mean, stats.variance, stats.skewness, stats.kurtosis]
+        return np.array(obs)
+
+    def AssembleAndSolve(self):
+        self.a.Assemble()
+        self.b.Assemble()
+        ess_tdof_list = intArray()
+        self.fespace.GetEssentialTrueDofs(self.ess_bdr, ess_tdof_list)
+        A = mfem.OperatorPtr()
+        B = mfem.Vector();  X = mfem.Vector()
+        self.a.FormLinearSystem(ess_tdof_list, self.x, self.b, A, X, B, 1)
+        AA = mfem.OperatorHandle2SparseMatrix(A)
+        M = mfem.GSSmoother(AA)
+        mfem.PCG(AA, M, B, X, -1, 200, 1e-12, 0.0)
+        self.a.RecoverFEMSolution(X,self.b,self.x)
+
+    def GetLocalErrors(self):
+        mfem_errors = self.estimator.GetLocalErrors()
+        errors = np.array([mfem_errors[i] for i in range(self.mesh.GetNE())])
+        return errors
+
+    def RenderMesh(self):
+        sol_sock = mfem.socketstream("localhost", 19916)
+        sol_sock.precision(8)
+        zerogf = mfem.GridFunction(self.fespace)
+        zerogf.Assign(0.0)
+        sol_sock.send_solution(self.mesh, zerogf)
+        title = "step " + str(self.k)
+        sol_sock.send_text('keys ARjlmp*******' + " window_title '" + title)
+
+    def UpdateMesh(self, action):
+        theta = action.item() # refinement threshold
+        if theta < 0. :
+          theta = 0.
+        if theta > 0.999 :
+          theta = 0.999 
+        self.Prefine(theta)
+        self.Refine(theta)
+
+    def Refine(self, theta):
+        # self.refiner.Reset()
+        self.refiner.SetTotalErrorFraction(theta)
+        self.refiner.Apply(self.mesh)
+        self.fespace.Update(False)
+        self.x.Update()
+        self.x.Assign(0.0)
+        self.x.ProjectBdrCoefficient(self.BC, self.ess_bdr)
+        # self.fespace.UpdatesFinished()
+        self.a.Update()
+        self.b.Update()
+
+    def Prefine(self, theta):
+        threshold = theta * np.max(self.errors)
+        for i in range(self.mesh.GetNE()):
+            if theta * threshold >= self.errors[i]:
+                current_order = self.fespace.GetElementOrder(i)
+                self.fespace.SetElementOrder(i, current_order + 1)
+        self.fespace.Update(False)
+        self.x.Update()
+        self.x.Assign(0.0)
+        self.x.ProjectBdrCoefficient(self.BC, self.ess_bdr)
+        # self.fespace.UpdatesFinished()
+        self.a.Update()
+        self.b.Update()
+    
+
+
+"""
+class PrefhpProblem(hpProblem):
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.action_space = spaces.Box(low=0.0, high=1.0, shape=(2,), dtype=np.float32)
+.
+    def UpdateMesh(self, action):
+        theta1 = action[0].item() # refine threshold
+        if theta1 < 0. :
+          theta1 = 0.
+        if theta1 > 0.999 :
+          theta1 = 0.999 
+        theta2 = action[1].item() # derefine threshold
+        theta2 *= theta1 # enforces deref < ref threshold
+        self.PRefine(theta1) 
+        self.Refine(theta1)
+"""        
+"""
+        rtransforms = self.mesh.GetRefinementTransforms()
+        coarse_to_fine = mfem.Table()
+        coarse_to_ref_type = mfem.intArray()
+        ref_type_to_matrix = mfem.Table()
+        ref_type_to_geom = mfem.GeometryTypeArray()
+        rtransforms.GetCoarseToFineMap(self.mesh, coarse_to_fine, coarse_to_ref_type, ref_type_to_matrix, ref_type_to_geom)
+        new_errors = mfem.doubleArray(coarse_to_fine.Width())
+        tmp = mfem.intArray(1)
+        for i in range(coarse_to_fine.Width()):
+            new_errors[i] = mfem.infinity()
+        for i in range(coarse_to_fine.Size()):
+            if coarse_to_fine.RowSize(i) == 1:
+                tmp_data = coarse_to_fine.GetRow(i)
+                tmp.Assign(tmp_data)
+                index = tmp[0]
+                new_errors[index] = self.errors[i]
+        self.mesh.DerefineByError(new_errors,threshold)
+        
+        # self.refiner.Reset()
+        self.fespace.Update()
+        self.x.Update()
+        # self.fespace.UpdatesFinished()
+        self.a.Update()
+        self.b.Update() """
